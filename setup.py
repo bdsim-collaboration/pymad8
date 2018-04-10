5a21e81ecef2a0598514c2e0393136ad88cd5b4a
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-04-10 18:24:15 +0200

    Update documentation and version number for release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymad8',
-    version='0.7',
+    version='1.0',
     packages=find_packages(exclude=["docs", "tests", "old"]),
     # Not sure how strict these need to be...
     install_requires=["matplotlib >= 1.7.1",
