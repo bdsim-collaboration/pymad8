0b9278826e0e64672e7146ab5d01cd69c85d0e6c
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2022-11-01 15:50:03 +0100

    Change to python 3 as a requirement.  Include licence file.

--- setup.py
+++ setup.py
@@ -4,17 +4,14 @@ setup(
     name='pymad8',
     version='1.6.1',
     packages=find_packages(exclude=["docs", "tests", "old"]),
-    # Not sure how strict these need to be...
-    install_requires=["matplotlib >= 1.7.1",
-                      "numpy >= 1.4.0",
-                      "fortranformat >= 0.2.5"],
-    # Some version of python2.7
-    python_requires=">=2.7.*",
-
+    install_requires=["matplotlib>=1.7.1",
+                      "numpy>=1.4.0",
+                      "fortranformat>=0.2.5"],
+    python_requires=">=3.7.*",
     author='JAI@RHUL',
     author_email='stewart.boogert@rhul.ac.uk',
     description="Write MAD8 models and load MAD8 output.",
-    url='https://bitbucket.org/jairhul/pymad8/',
     license='GPL3',
-    keywords='mad8 accelerator twiss'
+    url='https://bitbucket.org/jairhul/pymad8/',
+    keywords=['mad8', 'accelerator', 'twiss']
 )
